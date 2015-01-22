61933bdff3c17406e40b6bbb0374a72157ec4435
Tianon Gravi <admwiggin@gmail.com>
2015-01-22 11:38:55 -0700

    Add initial generate-stackbrew-library.sh script

---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+url='git://github.com/jfrazelle/irssi'
+
+echo '# maintainer: Jessie Frazelle <jess@docker.com> (@jfrazelle)'
+echo '# maintainer: Tianon Gravi <admwiggin@gmail.com> (@tianon)'
+
+commit="$(git log -1 --format='format:%H' -- .)"
+fullVersion="$(grep -m1 'ENV IRSSI_VERSION ' Dockerfile | cut -d' ' -f3)"
+
+versionAliases=()
+while [ "${fullVersion%.*}" != "$fullVersion" ]; do
+	versionAliases+=( $fullVersion )
+	fullVersion="${fullVersion%.*}"
+done
+versionAliases+=( $fullVersion latest )
+
+echo
+for va in "${versionAliases[@]}"; do
+	echo "$va: ${url}@${commit}"
+done
