46f8d10e03934bf9bd698999205013675e9c049d
Tianon Gravi <admwiggin@gmail.com>
2015-08-11 15:12:23 -0700

    Enhance "generate-stackbrew-library.sh" to only take into account changes to the Dockerfile or files from COPY in the Dockerfile for choosing the commit hash for a particular directory

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -8,7 +8,7 @@ url='git://github.com/jfrazelle/irssi'
 echo '# maintainer: Jessie Frazelle <jess@docker.com> (@jfrazelle)'
 echo '# maintainer: Tianon Gravi <admwiggin@gmail.com> (@tianon)'
 
-commit="$(git log -1 --format='format:%H' -- .)"
+commit="$(git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
 fullVersion="$(grep -m1 'ENV IRSSI_VERSION ' Dockerfile | cut -d' ' -f3)"
 
 versionAliases=()
