2065fe5f6c9656d34b301bbfd0643dcdbc17bd4b
Natanael Copa <ncopa@alpinelinux.org>
2016-01-15 09:41:30 +0100

    Add alpine variant

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+defaultVariant='debian'
+
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 url='git://github.com/jfrazelle/irssi'
@@ -8,17 +10,27 @@ url='git://github.com/jfrazelle/irssi'
 echo '# maintainer: Jessie Frazelle <jess@docker.com> (@jfrazelle)'
 echo '# maintainer: Tianon Gravi <admwiggin@gmail.com> (@tianon)'
 
-commit="$(git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
-fullVersion="$(git show "$commit:Dockerfile" | awk 'toupper($1) == "ENV" && $2 == "IRSSI_VERSION" { print $3 }')"
+for variant in debian alpine; do
+	commit="$(cd "$variant" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
+	fullVersion="$(git show "$commit:$variant/Dockerfile" | awk 'toupper($1) == "ENV" && $2 == "IRSSI_VERSION" { print $3 }')"
 
-versionAliases=()
-while [ "${fullVersion%.*}" != "$fullVersion" ]; do
-	versionAliases+=( $fullVersion )
-	fullVersion="${fullVersion%.*}"
-done
-versionAliases+=( $fullVersion latest )
+	versionAliases=()
+	while [ "${fullVersion%.*}" != "$fullVersion" ]; do
+		versionAliases+=( $fullVersion )
+		fullVersion="${fullVersion%.*}"
+	done
+	versionAliases+=( $fullVersion latest )
 
-echo
-for va in "${versionAliases[@]}"; do
-	echo "$va: ${url}@${commit}"
+	variantAliases=( "${versionAliases[@]/%/-$variant}" )
+	variantAliases=( "${variantAliases[@]//latest-/}" )
+	if [ "$variant" = "$defaultVariant" ]; then
+		versionAliases+=( "${variantAliases[@]}" )
+	else
+		versionAliases=( "${variantAliases[@]}" )
+	fi
+
+	echo
+	for va in "${versionAliases[@]}"; do
+		echo "$va: ${url}@${commit} $variant"
+	done
 done
