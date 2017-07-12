ce66aa9fac918b0285b95d36ae9e40999327a283
Tianon Gravi <admwiggin@gmail.com>
2017-07-12 12:59:03 -0700

    Ignore "-dev" also...

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-current="$(git ls-remote --tags https://github.com/irssi/irssi.git | cut -d/ -f3 | cut -d^ -f1 | sort -uV | grep -vE -- '-rc|-git$' | tail -1)"
+current="$(git ls-remote --tags https://github.com/irssi/irssi.git | cut -d/ -f3 | cut -d^ -f1 | sort -uV | grep -vE -- '-rc|-git$|-dev$' | tail -1)"
 
 set -x
 
